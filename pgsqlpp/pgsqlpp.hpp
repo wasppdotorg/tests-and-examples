117ca2335fc021c4b3f3e494bb6b0bee3afe4427
MobileGameServer <mobilegameserver@gmail.com>
2016-08-03 14:46:49 +0900

    released -> freed

--- pgsqlpp/pgsqlpp.hpp
+++ pgsqlpp/pgsqlpp.hpp
@@ -257,12 +257,12 @@ namespace pgsqlpp
 
 		std::tm* last_released()
 		{
-			return &released;
+			return &freed;
 		}
 
 		void set_released(const std::tm& released_)
 		{
-			released = released_;
+			freed = released_;
 		}
 
 		bool is_pooled()
@@ -278,7 +278,7 @@ namespace pgsqlpp
 	private:
 		st_pgsql* pgsql;
 
-		std::tm released;
+		std::tm freed;
 		bool pooled;
 
 	};
