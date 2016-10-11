6254417c64cdca351374abd7443c6cd7d70df407
MobileGameServer <mobilegameserver@gmail.com>
2016-10-11 18:11:23 +0900

    to string

--- curl/main.cpp
+++ curl/main.cpp
@@ -1,23 +1,37 @@
 
 #include <iostream>
+#include <string>
 
 #include <curl/curl.h>
+
+std::size_t curl_write_func(char* ptr, std::size_t size, std::size_t nmemb, void* userdata)
+{
+	((std::string*)userdata)->append(ptr, size * nmemb);
+	return size * nmemb;
+}
 
 int main()
 {
 	CURL* curl = curl_easy_init();
+	std::string html;
+
 	if (curl)
 	{
 		CURLcode res;
-		
 		curl_easy_setopt(curl, CURLOPT_URL, "http://www.boost.org/LICENSE_1_0.txt");
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_func);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &html);
+
 		res = curl_easy_perform(curl);
-		
-		
-		std::cout << (char*)res << std::endl;
-		curl_easy_cleanup(curl);
+		if (res == CURLE_OK)
+		{
+			std::cout << html << std::endl;
+			//std::cout << res << std::endl;
+		}
 	}
-	
+	curl_easy_cleanup(curl);
+
 	std::cin.get();
 	return 0;
 }
+
