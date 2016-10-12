a6470cd40609c8dcc8d9a6544e46b9a7e3ae213b
MobileGameServer <mobilegameserver@gmail.com>
2016-10-12 16:19:16 +0900

    curl ss

--- curl/main.cpp
+++ curl/main.cpp
@@ -13,19 +13,26 @@ std::size_t curl_write_func(char* ptr, std::size_t size, std::size_t nmemb, void
 int main()
 {
 	CURL* curl = curl_easy_init();
-	std::string html;
+
+	std::string url = "https://www.google.co.kr/?gws_rd=ssl";
+	std::string content;
 
 	if (curl)
 	{
-		curl_easy_setopt(curl, CURLOPT_URL, "http://www.boost.org/LICENSE_1_0.txt");
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+		if (url.substr(0, 5) == "https")
+		{
+			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+		}
+
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_func);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &html);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &content);
 
 		CURLcode res = curl_easy_perform(curl);
 		if (res == CURLE_OK)
 		{
-			std::cout << html << std::endl;
-			//std::cout << res << std::endl;
+			std::cout << content << std::endl;
 		}
 	}
 	curl_easy_cleanup(curl);
